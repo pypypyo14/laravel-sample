a4708f1d5893bf1b823e3983199f2439f17c8cd6
Saori <jkmr9214@gmail.com>
2019-09-08 13:46:39 +0900

    各種ディレクティブの練習

--- src/laravelapp/resources/views/hello/index.blade.php
+++ src/laravelapp/resources/views/hello/index.blade.php
@@ -19,5 +19,44 @@
         <input type="text" name="name">
         <input type="submit">
     </form>
+    @isset($data)
+    <p>&#064;foreachディレクティブ(& loopディレクティブ)の例</p>
+    <ol>
+        @foreach($data as $item)
+            @if ($loop->first)
+            <p>※ データ一覧</p>
+            @endif
+            <li>No. {{$loop->iteration}}. {{$item}}</li>
+            @if ($loop->last)
+            <p>-- ここまで --</p>
+            @endif
+        @endforeach
+    </ol>
+    @endisset
+    <p>&#064;forディレクティブの例</p>
+    <ol>
+    @for ($i=1;$i<100;$i++)
+    @if ($i %2 == 1)
+        @continue
+    @elseif($i <=10)
+        <li>No. {{$i}}</li>
+    @else
+        @break
+    @endif
+    @endfor
+    </ol>
+
+    <p>&#064;whileディレクティブ(& phpディレクティブ)の例</p>
+    <ol>
+        @php
+            $counter = 0;
+        @endphp
+        @while ($counter<count($data))
+            <li>{{$data[$counter]}}</li>
+            @php
+                $counter++;
+            @endphp
+        @endwhile
+    </ol>
 </body>
 </html>
