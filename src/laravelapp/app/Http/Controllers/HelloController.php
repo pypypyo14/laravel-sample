350532026060040d8d0e494c3e97abfb6c5f8a6f
Saori <jkmr9214@gmail.com>
2019-09-08 16:40:24 +0900

    バリデータ実装

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -5,14 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request){
-        return view('hello.index', ['msg'=>'フォームを入力：']);
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator->fails()){
+            $msg = 'クエリに問題があります。';
+        } else {
+            $msg = 'id, passを受け付けました。フォームを入力ください。';
+        }
+        return view('hello.index', ['msg'=>$msg, ]);
     }
 
-    public function post(HelloRequest $request){
+    public function post(Request $request){
+        $rules = [
+            'name'=>'required',
+            'mail'=>'email',
+            'age'=>'numeric|between:0,150',
+        ];
+        $messages = [
+            'name.required'=>'名前は必ず入力してください。',
+            'mail.email'=>'メールアドレスが必要です。',
+            'age.numeric'=>'年齢を整数で記入ください。',
+            'age.between'=>'年齢を0-150の間で入力ください。',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+        if ($validator->fails()){
+            return redirect('hello')
+                    -> withErrors($validator)
+                    ->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
