aad5428b82424f450b33083c83f7ea75d4747aca
Saori <jkmr9214@gmail.com>
2019-09-08 14:25:32 +0900

    @eachでコンポーネントを繰り返し表示

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -8,11 +8,11 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index(){
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachiko@happy'],
+        ];
         return view('hello.index', ['data'=>$data]);
-    }
-
-    public function post(Request $request){
-        return view('hello.index', ['name'=>$request->name]);
     }
 }
--- src/laravelapp/resources/views/hello/index.blade.php
+++ src/laravelapp/resources/views/hello/index.blade.php
@@ -10,16 +10,9 @@
 
 @section('content')
     <p>ここが本文のコンテンツ</p>
-    <p>必要なだけ記述可能</p>
-    @component('components.message')
-        @slot('msg_title')
-            CAUTION!
-        @endslot
-        @slot('msg_content')
-            これはコンポーネントの表示です
-        @endslot
-    @endcomponent
-    @include('components.message',['msg_title'=>'OK', 'msg_content'=>'これはサブビューの表示です'])
+    <ul>
+        @each('components.item', $data, 'item')
+    </ul>
 @endsection
 
 @section('footer')
