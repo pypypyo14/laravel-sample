0f3a980bc3a79ec91b76516e6818a773f3832956
Saori <jkmr9214@gmail.com>
2019-09-08 14:46:42 +0900

    use view composer

--- src/laravelapp/config/app.php
+++ src/laravelapp/config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // add pypypyo14
+        App\Providers\HelloServiceProvider::class,
+
     ],
 
     /*
