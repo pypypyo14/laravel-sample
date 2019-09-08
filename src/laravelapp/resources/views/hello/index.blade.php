28b86422ff42d42a572ac404bb55aed448d19275
Saori <jkmr9214@gmail.com>
2019-09-08 14:15:07 +0900

    コンポーネントの利用

--- src/laravelapp/resources/views/hello/index.blade.php
+++ src/laravelapp/resources/views/hello/index.blade.php
@@ -11,6 +11,14 @@
 @section('content')
     <p>ここが本文のコンテンツ</p>
     <p>必要なだけ記述可能</p>
+    @component('components.message')
+        @slot('msg_title')
+            CAUTION!
+        @endslot
+        @slot('msg_content')
+            これはメッセージの表示です
+        @endslot
+    @endcomponent
 @endsection
 
 @section('footer')
