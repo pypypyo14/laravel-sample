0c74625cdac4a7c5e2d0407a8c00048f9aa6aa59
Saori <jkmr9214@gmail.com>
2019-09-09 23:02:10 +0900

    fix typo

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -11,7 +11,7 @@ class HelloController extends Controller
 {
     public function index(Request $request){
         if ($request->hasCookie('msg')){
-            $msg = 'Cookie: '. $request->cookir('msg');
+            $msg = 'Cookie: '. $request->cookie('msg');
         } else {
             $msg = 'Cookieはありません';
         }
