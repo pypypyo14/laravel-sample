ccddbc8e1d8986226d775cb022122df6e71d993f
Saori <jkmr9214@gmail.com>
2019-09-10 22:54:31 +0900

    INSERT, UPDATE

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -19,4 +19,38 @@ class HelloController extends Controller
         }
         return view('hello.index', ['items'=> $items]);
     }
+
+    public function add(Request $request){
+        return view('hello.add');
+    }
+
+    public function create(Request $request){
+        $param = [
+            'name' => $request -> name,
+            'mail' => $request -> mail,
+            'age' => $request  -> age,
+        ];
+        DB::insert(
+            'insert into people (name, mail, age)
+                values (:name, :mail, :age)', $param );
+        return redirect('hello');
+    }
+
+    public function edit(Request $request){
+        $param = ['id' => $request -> id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.edit', ['form'=> $item[0]]);
+    }
+
+    public function update(Request $request){
+        $param = [
+            'id'   => $request -> id,
+            'name' => $request -> name,
+            'mail' => $request -> mail,
+            'age'  => $request -> age,
+        ];
+        DB::update('update people set name = :name, mail = :mail, age = :age where id = :id', $param);
+        return redirect('hello');
+    }
+
 }
--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
 
 use App\Http\Middleware\HelloMiddleware;
 
-Route::get('hello', 'Hellocontroller@index');
+Route::get('hello' , 'HelloController@index');
 Route::post('hello', 'HelloController@post');
 Route::get('single', 'SingleActionController');
+
+Route::get('hello/add' , 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+
+Route::get('hello/edit' , 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
