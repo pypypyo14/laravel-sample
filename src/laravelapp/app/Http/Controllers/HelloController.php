c99a3a5642b479fb902ffda579478eb697cf847c
Saori <jkmr9214@gmail.com>
2019-09-11 00:08:09 +0900

    update, delete文をクエリビルダを用いたものに変更

--- src/laravelapp/app/Http/Controllers/HelloController.php
+++ src/laravelapp/app/Http/Controllers/HelloController.php
@@ -32,9 +32,10 @@ class HelloController extends Controller
     }
 
     public function edit(Request $request){
-        $param = ['id' => $request -> id];
-        $item = DB::table('people')->get();
-        return view('hello.edit', ['form'=> $item[0]]);
+        $item = DB::table('people')
+                    -> where('id', $request->id)
+                    -> first();
+        return view('hello.edit', ['form'=> $item]);
     }
 
     public function update(Request $request){
@@ -44,19 +45,25 @@ class HelloController extends Controller
             'mail' => $request -> mail,
             'age'  => $request -> age,
         ];
-        DB::update('update people set name = :name, mail = :mail, age = :age where id = :id', $param);
+        DB::table('people')
+            -> where('id', $request->id)
+            -> update($param);
         return redirect('hello');
     }
 
     public function del(Request $request){
         $param = ['id' => $request->id];
-        $item = DB::table('people')->get();
-        return view('hello.del', ["form" => $item[0]]);
+        $item = DB::table('people')
+                ->where('id', $request->id)
+                ->first();
+        return view('hello.del', ["form" => $item]);
     }
 
     public function remove(Request $request){
         $param = ['id' => $request -> id];
-        DB::delete('delete from people where id = :id', $param);
+        DB::table('people')
+                -> where('id', $request->id)
+                -> delete();
         return redirect('hello');
     }
 
